e013cbf14d0a9a62ce7fabea6b686a4a3cc2746e
GabrielGomes856 <gabrielgomes200610@gmail.com>
2025-05-05 23:30:24 -0300

    primeiro commit

---
+++ app/index.tsx
@@ -0,0 +1,36 @@
+import { StyleSheet, View } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { WebView } from "react-native-webview";
+
+const URL = "http://gzbriel72.github.io/farma-med-web/";
+
+export default function HomeScreen() {
+  const insets = useSafeAreaInsets();
+
+  return (
+    <View style={[styles.container, { paddingTop: insets.top }]}>
+      <WebView
+        source={{ uri: URL }}
+        style={styles.webview}
+        javaScriptEnabled={true}
+        domStorageEnabled={true}
+        startInLoadingState={true}
+        scalesPageToFit={true}
+        onError={(syntheticEvent) => {
+          const { nativeEvent } = syntheticEvent;
+          console.warn("WebView error: ", nativeEvent);
+        }}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+  webview: {
+    flex: 1,
+  },
+});
